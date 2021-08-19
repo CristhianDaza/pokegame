1bd56c9b237624cda41eff77302adc978f6a2255
Cristhian Daza <cristian.d.b@hotmail.com>
2021-08-18 22:53:19 -0500

    Agregando estilos y base de componentes

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,5 +1,12 @@
 <template>
-  <h3>Pokemon Options</h3>
+  <div class="options-container">
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -7,3 +14,29 @@ export default {
   name: "PokemonOptions"
 }
 </script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+    margin-top: 50px;
+  }
+  li {
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    margin-bottom: 10px;
+    width: 250px;
+    transition: all .2s ease;
+    color: #0d1117;
+  }
+
+  li:hover {
+    background-color: rgba(255, 255, 255, 0.5);
+  }
+
+  .options-container {
+    display: flex;
+    justify-content: center;
+  }
+</style>
--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,5 +1,16 @@
 <template>
-  <h3>Pokemon picture</h3>
+  <div class="pokemon-container">
+    <img
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"
+        alt="Pokemon"
+        class="hidden-pokemon"
+    >
+<!--    <img
+       src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/4.svg"
+       class="fade-in"
+       alt="Pokemon"
+> -->
+  </div>
 </template>
 
 <script>
@@ -7,3 +18,22 @@ export default {
   name: "PokemonPicture"
 }
 </script>
+
+<style scoped>
+  .pokemon-container {
+    height: 200px;
+  }
+  img {
+    height: 200px;
+    position: absolute;
+    /*right: 32%;*/
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+  }
+  .hidden-pokemon {
+    filter: brightness(0);
+  }
+</style>
