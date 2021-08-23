16ee216e90c25efd293f0c6d0e092218f1a9a359
Cristhian Daza <cristian.d.b@hotmail.com>
2021-08-23 08:15:12 -0500

    Agregando prueba de checkAnswer

--- tests/unit/pages/pokemonPage.spec.js
+++ tests/unit/pages/pokemonPage.spec.js
@@ -50,4 +50,27 @@ describe('pokemonPage component', () => {
         expect(picture.attributes('pokemonid')).toBe('1')
         expect(options.attributes('pokemons')).toBeTruthy()
     })
+
+    test('pruebas con checkAnswer',async () => {
+        const wrapper = shallowMount(PokemonPage, {
+            data() {
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: ''
+                }
+            }
+        })
+
+        await wrapper.vm.checkAnswer(1)
+
+        expect(wrapper.find('h2').exists()).toBeTruthy()
+        expect(wrapper.vm.showPokemon).toBeTruthy()
+        expect(wrapper.find("h2").text()).toBe(`Correcto, ${ pokemons[0].name }`)
+
+        await wrapper.vm.checkAnswer(10)
+        expect(wrapper.vm.message).toBe(`Ooops, era ${ pokemons[0].name }`)
+    })
 })
