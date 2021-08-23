f30d216431a1c047d1791d819be90c1e3a43b98e
Cristhian Daza <cristian.d.b@hotmail.com>
2021-08-23 07:52:54 -0500

    Agregando snapshot

--- tests/unit/pages/pokemonPage.spec.js
+++ tests/unit/pages/pokemonPage.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount, mount } from '@vue/test-utils'
 import PokemonPage from '@/pages/PokemonPage'
+import { pokemons } from '../mocks/pokemons.mock'
 
 describe('pokemonPage component', () => {
     let wrapper
@@ -10,5 +11,21 @@ describe('pokemonPage component', () => {
         const mixPokemonArraySpy = jest.spyOn(PokemonPage.methods, 'mixPokemonArray')
         shallowMount(PokemonPage)
         expect(mixPokemonArraySpy).toHaveBeenCalled()
+    })
+
+    test('debe de hacer match con el snapshot cuando cargan los pokemons', () => {
+        const wrapper = mount(PokemonPage, {
+            data() {
+                return {
+                    pokemonArr: pokemons,
+                    pokemon: pokemons[0],
+                    showPokemon: false,
+                    showAnswer: false,
+                    message: ''
+                }
+            }
+        })
+
+        expect(wrapper.html()).toMatchSnapshot()
     })
 })
